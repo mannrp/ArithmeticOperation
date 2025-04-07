280cace131bd50b913fb62b80e0e73e494939f5e
mannrp <141443279+mannrp@users.noreply.github.com>
2025-04-06 21:31:40 -0400

    test to Fail

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -12,7 +12,7 @@ public class ArithmeticOperationsTest {
 	{
 
 		ArithmeticOperations operations = new ArithmeticOperations();
-		Integer actual = operations.addOrSub(2, 6);
+		Integer actual = operations.addOrSub(3, 6);
 		Integer expected = 8;
 		assertEquals(expected, actual);
 
